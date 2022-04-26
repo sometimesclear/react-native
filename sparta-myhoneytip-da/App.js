806e79f3d01081140d47fe9b0b6c0c9fba870ec1
ldhldh <ldhldh@inforang.com>
2022-04-26 15:33:37 +0900

    220426-img-style

--- sparta-myhoneytip-da/App.js
+++ sparta-myhoneytip-da/App.js
@@ -1,23 +1,28 @@
 //import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Button, Alert, Image, LayoutAnimation } from 'react-native';
+import favicon from "./assets/favicon.png"
 
 export default function App() {
   return (
     <ScrollView>
       <View style={styles.container}>
         <View style={styles.subContainerOne}>
-          <Text style={styles.mainText}>텍스트는 Text 태그 안에 넣어요.</Text>
-        </View>
+          <Image
+            source={favicon}
+            resizeMode={"center"}
+            style={styles.imageStyle}
+          />
+        </View>       
         <View style={styles.subContainerOne}>
-          <Text style={styles.mainText}>텍스트는 Text 태그 안에 넣어요.</Text>
-        </View>
-        <View style={styles.subContainerOne}>
-          <Text style={styles.mainText}>텍스트는 Text 태그 안에 넣어요.</Text>
-        </View>
-        <View style={styles.subContainerOne}>
-          <Text style={styles.mainText}>텍스트는 Text 태그 안에 넣어요.</Text>
-        </View>
+          <Image
+            style={styles.imageStyle}
+            source={{
+              uri: 'https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80'
+            }}
+            resizeMode={"cover"}
+           />            
+        </View>        
         <Button 
           style={styles.buttonStyle}
           title="파랑 버튼"
@@ -35,20 +40,20 @@ export default function App() {
 
         <View style={styles.subContainerOne}>
           <Text style={styles.mainText}>텍스트는 Text 태그 안에 넣어요.</Text>
-        </View>
-        <View style={styles.subContainerOne}>
-          <Text style={styles.mainText}>텍스트는 Text 태그 안에 넣어요.</Text>
-        </View>        
-        <View style={styles.subContainerOne}>
-          <Text style={styles.mainText}>텍스트는 Text 태그 안에 넣어요.</Text>
-        </View>
-        <View style={styles.subContainerOne}>
-          <Text style={styles.mainText}>텍스트는 Text 태그 안에 넣어요.</Text>
-        </View>
-        <View style={styles.subContainerOne}>
-          <Text style={styles.mainText}>텍스트는 Text 태그 안에 넣어요.</Text>
-        </View>        
-        <View style={styles.subContainerTwo}></View>      
+        </View>                     
+        
+        <View style={styles.subContainerTwo}>
+          <Text style={styles.subText}>서브 텍스트, 서브 텍스트</Text>
+          <Text style={styles.subText2}>서브 텍스트2, 서브 텍스트2</Text>
+          <View style={styles.subContainerTwo}> 
+            <Image
+              source={favicon}
+              resizeMode={"center"}
+              style={styles.imageStyle}
+            />   
+          </View>
+        </View>   
+        
       </View>
     </ScrollView>
   );
@@ -60,21 +65,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',   
   },
   subContainerOne: {
-    height:100,
+    height:200,
     borderColor:'#000',
-    borderWidth:1,
-    borderRadius:10,
+    borderWidth:1,    
     margin:10,
-    padding:20,
+    padding:10,
     textAlign:"center",
   },
   subContainerTwo: {
     flex:1,
-    backgroundColor:"blue",
+    flexDirection: "row",      
+    //backgroundColor:"blue",
   },
 
   buttonStyle: {
     margin:10,
+    width:200,
+  },
+
+  imageStyle: {
+    width:"100%",
+    height:"100%",
+    alignItems:"center",
+    justifyContent:"center",
+  },
+
+  subText: {
+    backgroundColor:"#4caf50",
+    color:"#ffffff",
+    padding:10,  
   },
   
+  subText2: {
+    backgroundColor:"#ff7f50",
+    padding:10,  
+  },
 });
